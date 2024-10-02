e22bc469c4b75ad8e6e59223c05d171190530442
Hen Cohen <hencohen83@gmail.com>
2024-10-03 02:40:23 +0300

    db working soon

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from functools import wraps
 import jwt
 import datetime
 
@@ -9,31 +9,41 @@ app.config.from_object("config.Config")
 db = SQLAlchemy(app)
 
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.headers.get("x-access-tokens")
-        if not token:
-            return jsonify({"message": "Token is missing!"}), 403
-        try:
-            data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
-        except:
-            return jsonify({"message": "Token is invalid!"}), 403
-        return f(*args, **kwargs)
-
-    return decorated
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
 
-@app.route("/unprotected")
-def unprotected():
-    return jsonify({"message": "Anyone can view this!"})
+@app.route("/register", methods=["POST"])
+def signup_user():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data["password"], method="pbkdf2:sha256")
+    new_user = User(username=data["username"], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "registered successfully"})
 
 
-@app.route("/protected")
-@token_required
-def protected():
-    return jsonify({"message": "This is only available for people with valid tokens."})
+@app.route("/login", methods=["POST"])
+def login_user():
+    data = request.get_json()
+    user = User.query.filter_by(username=data["username"]).first()
+    if user and check_password_hash(user.password, data["password"]):
+        token = jwt.encode(
+            {
+                "username": user.username,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            },
+            app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+        return jsonify({"token": token})
+    return make_response(
+        "could not verify", 401, {"WWW-Authenticate": 'Basic realm="Login required!"'}
+    )
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
